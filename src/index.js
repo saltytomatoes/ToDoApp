213235235ac254b891c4068f5484b4451e6cdf57
saltytomatoes <saltytomatoes1337@gmail.com>
2021-02-26 14:19:40 +0200

    Designed header and project holder

--- src/index.js
+++ src/index.js
@@ -4,4 +4,36 @@ const InitialContent = (function () {
 
     const Content = document.querySelector("#Content");
 
+    const header = `<header>
+                        <span>J</span>
+                        <span>u</span>
+                        <span>s</span>
+                        <span>t</span>&nbsp&nbsp
+                        <span>F</span>
+                        <span>*</span>
+                        <span>c</span>
+                        <span>k</span>
+                        <span>i</span>
+                        <span>n</span>
+                        <span>g</span>&nbsp&nbsp
+                        <span>D</span>
+                        <span>o</span>&nbsp&nbsp
+                        <span>I</span>
+                        <span>t</span>
+                    </header>`;
+                    
+    const projectHolder = `<div id="projectHolder">
+                            
+                          </div>`;
+
+    const mainHolder = `<div id="mainHolder">
+                          ${projectHolder}
+                        </div>`;
+
+
+    Content.innerHTML = `
+                        ${header}
+                        ${mainHolder}
+                        `;
+
 })();
