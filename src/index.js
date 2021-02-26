5d3f8979db5aa4d07a9d38cde1a4d737320b21d6
saltytomatoes <saltytomatoes1337@gmail.com>
2021-02-26 23:34:13 +0200

    designed top bar of project holder, added project object

--- src/index.js
+++ src/index.js
@@ -1,31 +1,78 @@
 
+import { EventEmitter } from './eventEmitter.js';
 
-const InitialContent = (function () {
+// Converts string to dom elements
+function strToHtml(str) {
+    let parser = new DOMParser();
+    str = parser.parseFromString(str, "text/html").body.firstElementChild;
+
+    return str;
+}
+
+
+// sets the initial view
+const InitializeView = (function () {
 
     const Content = document.querySelector("#Content");
 
-    const header = `<header>
-                        <span>Just</span> <span>F*cking</span> <span>Do</span> <span>It</span>
-                    </header>`;
-                    
-    const projectHolder = `<div id="projectHolder" class="subdiv">
-                            <h1 class="title">Projects</h1>
-                            <hr>
-                          </div>`;
-
-    const projectMonitor = `<div id="projectMonitor" class="subdiv">
-                                <h1 class="title">--proj name--</h1>
-                                <hr>
-                            </div>`;
-    const mainHolder = `<div id="mainHolder">
-                          ${projectHolder}
-                          ${projectMonitor}
-                        </div>`;
+    const header = strToHtml(`
+                                <header>
+                                <span>Just</span> <span>F*cking</span> <span>Do</span> <span>It</span>
+                                </header>
+                            `);
 
 
-    Content.innerHTML = `
-                        ${header}
-                        ${mainHolder}
-                        `;
+    const projectHolder = strToHtml(`
+                                        <div id="projectHolder" class="subdiv">
+                                            <div class="topBar">
+                                                <h1 class="title">Projects</h1>
+                                                <div id="addProjectBtn" class="notTxt">+</div>
+                                            </div>
+                                            <hr>
+                                        </div>
+                                    `);
+
+
+
+    const projectMonitor = strToHtml(`
+                                        <div id="projectMonitor" class="subdiv">
+                                            <h1 class="title">--proj name--</h1>
+                                            <hr>
+                                        </div>`
+                                    );
+
+
+    const mainHolder = strToHtml(`<div id="mainHolder"></div>`);
+    mainHolder.appendChild(projectHolder);
+    mainHolder.appendChild(projectMonitor);
+
+
+
+    Content.appendChild(header);
+    Content.appendChild(mainHolder);
+
+    return {
+        projectHolder,
+        projectMonitor
+    }
+
+})();
+
+
+// 
+const App = (function () {
+
+    function Project(name) {
+        this.name = name;
+        this.dom = strToHtml(`<div class="project">${this.name}</div>`);
+
+        return this.dom;
+    }
+
+    InitializeView.projectHolder.appendChild(new Project("hello test"));
+    InitializeView.projectHolder.appendChild(new Project("hello"));
+    InitializeView.projectHolder.appendChild(new Project(" test"));
+
+
 
 })();
